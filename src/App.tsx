95d8c4ad170c20c78ee95f9d14997ada27810143
Jonathan Fillipe Lemes <jlemes@findmyprofession.com>
2022-05-02 01:03:44 -0300

    feat: 🎸 words and letters states created

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import { Flex, SimpleGrid, Box, Text } from '@chakra-ui/react'
+import {
+  Flex,
+  SimpleGrid,
+  Box,
+  Text,
+  Button,
+  Grid,
+  GridItem,
+} from '@chakra-ui/react'
 
 function App() {
-  const [letters, setLetters] = useState(['', '', '', '', ''])
+  const [selectedLetter, setSelectedLetter] = useState<number>(0)
+  const [selectedWord, setSelectedWord] = useState<number>(0)
+  const [firstWord, setFirstWord] = useState<string[]>(['', '', '', '', ''])
+  const [secondWord, setSecondWord] = useState<string[]>(['', '', '', '', ''])
+  const [thirdWord, setThirdWord] = useState<string[]>(['', '', '', '', ''])
+  const [fourthWord, setFourthWord] = useState<string[]>(['', '', '', '', ''])
+  const [fifthWord, setFifthWord] = useState<string[]>(['', '', '', '', ''])
 
-  const letterBox = (letter: string, index: number) => {
+  const letterBox = (letter: string, index: number, word: number) => {
     return (
       <Box
         border="solid rgba(0, 0, 0, 0.4) 6px"
         borderRadius="8px"
+        borderStyle={
+          selectedLetter === index && word === selectedWord ? 'groove' : 'solid'
+        }
         key={index}
         m="8px"
         height="80px"
@@ -31,6 +48,24 @@ function App() {
     )
   }
 
+  const keyBoardLetter = (letter: string, colSpan?: number) => {
+    return (
+      <GridItem colSpan={colSpan || 1}>
+        <Button
+          variant="outline"
+          colorScheme="gray"
+          color="white"
+          fontWeight="bold"
+          fontSize="3xl"
+          height="64px"
+          minWidth="64px"
+        >
+          {letter}
+        </Button>
+      </GridItem>
+    )
+  }
+
   return (
     <Flex
       bgColor="#282c34"
@@ -41,13 +76,48 @@ function App() {
       <Flex flexDirection="column" width="720px">
         <Flex height="120px"></Flex>
         <SimpleGrid columns={5} mx="120px" height="calc(100vh - 370px)">
-          {letters.map((letter, index) => letterBox(letter, index))}
-          {letters.map((letter, index) => letterBox(letter, index))}
-          {letters.map((letter, index) => letterBox(letter, index))}
-          {letters.map((letter, index) => letterBox(letter, index))}
-          {letters.map((letter, index) => letterBox(letter, index))}
+          {firstWord.map((letter, index) => letterBox(letter, index, 0))}
+          {secondWord.map((letter, index) => letterBox(letter, index, 1))}
+          {thirdWord.map((letter, index) => letterBox(letter, index, 2))}
+          {fourthWord.map((letter, index) => letterBox(letter, index, 3))}
+          {fifthWord.map((letter, index) => letterBox(letter, index, 4))}
         </SimpleGrid>
-        <Flex height="250px"></Flex>
+        <Grid
+          gap={2}
+          templateColumns="repeat(10, 1fr)"
+          templateRows="repeat(3, 1fr)"
+          height="250px"
+          width="100%"
+        >
+          {keyBoardLetter('Q')}
+          {keyBoardLetter('W')}
+          {keyBoardLetter('E')}
+          {keyBoardLetter('R')}
+          {keyBoardLetter('T')}
+          {keyBoardLetter('Y')}
+          {keyBoardLetter('U')}
+          {keyBoardLetter('I')}
+          {keyBoardLetter('O')}
+          {keyBoardLetter('P')}
+          {keyBoardLetter('A')}
+          {keyBoardLetter('S')}
+          {keyBoardLetter('D')}
+          {keyBoardLetter('F')}
+          {keyBoardLetter('G')}
+          {keyBoardLetter('H')}
+          {keyBoardLetter('J')}
+          {keyBoardLetter('K')}
+          {keyBoardLetter('L')}
+          {keyBoardLetter('BKSPC')}
+          {keyBoardLetter('Z')}
+          {keyBoardLetter('X')}
+          {keyBoardLetter('C')}
+          {keyBoardLetter('V')}
+          {keyBoardLetter('B')}
+          {keyBoardLetter('N')}
+          {keyBoardLetter('M')}
+          {keyBoardLetter('ENTER', 2)}
+        </Grid>
       </Flex>
     </Flex>
   )
